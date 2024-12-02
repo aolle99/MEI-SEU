1f073119254ac95d79f16c0ab793e9e45feccf2a
Àlex Ollé <aolle99@gmail.com>
2024-12-02 19:06:32 +0100

    lab4

--- Laboratoris/4. CAN bus/python_sender.py
+++ Laboratoris/4. CAN bus/python_sender.py
@@ -1,7 +1,22 @@
 import time
+
 import pyautogui
 from gs_usb.gs_usb import GsUsb
 from gs_usb.gs_usb_frame import GsUsbFrame
+
+
+# gs_usb general also can import from gs_usb_structures.py
+GS_USB_ECHO_ID = 0
+GS_USB_NONE_ECHO_ID = 0xFFFFFFFF
+
+
+
+GS_CAN_MODE_NORMAL = 0
+GS_CAN_MODE_LISTEN_ONLY = (1 << 0)
+GS_CAN_MODE_LOOP_BACK = (1 << 1)
+#GS_CAN_MODE_TRIPLE_SAMPLE = (1 << 2)
+GS_CAN_MODE_ONE_SHOT = (1 << 3)
+GS_CAN_MODE_HW_TIMESTAMP = (1 << 4)
 
 def main():
     # Inicializar el dispositivo CAN
@@ -16,29 +31,51 @@ def main():
     if not dev.set_bitrate(500000):
         print("No se puede establecer el bitrate para gs_usb")
         return
-    dev.start(GS_CAN_MODE_NORMAL)
+    dev.start(GS_CAN_MODE_LOOP_BACK)
+
+    player_id=0
 
     # Obtener las dimensiones de la pantalla
     screen_width, screen_height = pyautogui.size()
+    posx = 0
+    posy=0
+    direction = 1
 
     while True:
-        # Obtener la posición actual del ratón
-        x, y = pyautogui.position()
+        iframe = GsUsbFrame()
+        dev.read(iframe, 1)
+        if player_id == 0:
+            # Obtener la posición actual del ratón
+            x, y = pyautogui.position()
 
-        # Convertir las coordenadas a valores entre 0 y 254
-        x_scaled = int((x / screen_width) * 254)
-        y_scaled = int((y / screen_height) * 254)
+            # Convertir las coordenadas a valores entre 0 y 254
+            x_scaled = int((x / screen_width) * 254)
+            y_scaled = int((1 - (y / screen_height)) * 254)
 
-        # Crear el mensaje CAN
-        data = bytes([x_scaled, y_scaled, 0, 0, 0, 0, 0, 0])
-        frame = GsUsbFrame(can_id=0x00, data=data)
+            # Crear el mensaje CAN
+            data = bytes([x_scaled, y_scaled, 0, 0, 0, 0, 0, 0])
+            frame = GsUsbFrame(can_id=0x00, data=data)
 
-        # Enviar el mensaje CAN
-        if dev.send(frame):
-            print(f"TX  CAN ID: 0x00, X: {x_scaled}, Y: {y_scaled}")
+            # Enviar el mensaje CAN
+            if dev.send(frame):
+                print(f"TX  CAN ID: 0x00, X: {x_scaled}, Y: {y_scaled}")
+        else:
+            posx += direction
+            posy += direction
+            # Convertir las coordenadas a valores entre 0 y 254
+            if posx >= 254 or posx <= 0:
+                direction *= -1
+            # Crear el mensaje CAN
+            data = bytes([posx, posy, 0, 0, 0, 0, 0, 0])
+            frame = GsUsbFrame(can_id=1, data=data)
+
+            # Enviar el mensaje CAN
+            if dev.send(frame):
+                print(f"TX  CAN ID: 0x01, X: {posx}, Y: {posy}")
 
         # Esperar un poco antes de la siguiente actualización
-        time.sleep(0.1)
+        time.sleep(0.001)
+        #player_id = 1 - player_id
 
 if __name__ == "__main__":
     try:
--- Laboratoris/4. CAN bus/segonapart.cpp
+++ Laboratoris/4. CAN bus/segonapart.cpp
@@ -1,30 +1,38 @@
 #include "mbed.h"
+#include <cstdio>
 
 #define CAN_BAUDRATE 500000
-#define DELAY_US 150
+#define DELAY_US 10
+#define PERIOD 0.001f
 
 CAN can(p30, p29, CAN_BAUDRATE);  // CAN RX, CAN TX
 AnalogOut OUTX(p18);  // Salida analógica para el primer punto
-AnalogOut OUTY(p19);  // Salida analógica para el segundo punto
-
+PwmOut OUTY(p21);  // Salida analógica para el segundo punto
+DigitalOut led(LED1);
 int main() {
     CANMessage msg;
+    OUTY.period(PERIOD);
     while (1) {
-        
         if (can.read(msg)) {
             if (msg.id == 0x00 || msg.id == 0x01) {
                 float x = static_cast<float>(msg.data[0]) / 255.0f;
                 float y = static_cast<float>(msg.data[1]) / 255.0f;
-                
-                if (msg.id == 0x00) {
+                if(msg.id == 0x00) {
+                    printf("Jugador 0: X: %d Y: %d \n", (int)(x*100),(int)(y*100));
+
                     OUTX.write(x);
                     OUTY.write(y);
-                } else {  // msg.id == 0x01
-                    OUTX.write(y);
-                    OUTY.write(x);
                 }
+                else if(msg.id == 0x01) {
+                    printf("Jugador 0: X: %d Y: %d \n", (int)(x*100),(int)(y*100));
+
+                    OUTX.write(x);
+                    OUTY.write(y);
+                }
+                
+                
             }
         }
-        delay_us(DELAY_US);
+        wait_us(DELAY_US);
     }
 }
